d94d31bce2cf2403eec2dbf25d5e50927e9d6878
Navaneeth R <navaneethrenjth1609@gmail.com>
2020-12-24 16:20:29 +0530

    improved chart file

--- Medwise/lib/widgets/chart.dart
+++ Medwise/lib/widgets/chart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   // List<Map<String,Object>> get recentTasks {
@@ -23,254 +22,80 @@ class Chart extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Column(
-              //6 days back
-              children: [
-                Text('S'),
-                SizedBox(height: 4),
-                Container(
-                  height: 60,
-                  width: 10,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        heightFactor: 0.8,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffB83F87),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ChartBar(
+              title: 'S',
+              height: 0.8,
             ),
-            Column(
-              //5 days back
-              children: [
-                Text('S'),
-                SizedBox(height: 4),
-                Container(
-                  height: 60,
-                  width: 10,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        heightFactor: 0.9,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffB83F87),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ChartBar(
+              title: 'S',
+              height: 0.9,
             ),
-            Column(
-              //4 days back
-              children: [
-                Text('M'),
-                SizedBox(height: 4),
-                Container(
-                  height: 60,
-                  width: 10,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        heightFactor: 0.8,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffB83F87),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ChartBar(
+              title: 'M',
+              height: 0.8,
             ),
-            Column(
-              //3 days back
-              children: [
-                Text('T'),
-                SizedBox(height: 4),
-                Container(
-                  height: 60,
-                  width: 10,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        heightFactor: 1,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffB83F87),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ChartBar(
+              title: 'T',
+              height: 1,
             ),
-            Column(
-              //2 days back
-              children: [
-                Text('W'),
-                SizedBox(height: 4),
-                Container(
-                  height: 60,
-                  width: 10,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        heightFactor: 0.85,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffB83F87),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ChartBar(
+              title: 'W',
+              height: 0.85,
             ),
-            Column(
-              //1 day back
-              children: [
-                Text('T'),
-                SizedBox(height: 4),
-                Container(
-                  height: 60,
-                  width: 10,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        heightFactor: 1,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffB83F87),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ChartBar(
+              title: 'T',
+              height: 1,
             ),
-            Column(
-              //Today
-              children: [
-                Text('F'),
-                SizedBox(height: 4),
-                Container(
-                  height: 60,
-                  width: 10,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        heightFactor: 0.2,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffB83F87),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ChartBar(
+              title: 'F',
+              height: 0.2,
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ChartBar extends StatelessWidget {
+  ChartBar({this.title, this.height});
+  final String title;
+  final double height;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(title),
+        SizedBox(height: 4),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: height,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffB83F87),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
